bbc6197c00b6c23d249c32720cee0ae8c9a40950
Javad Shafique <javadshafique@hotmail.com>
2026-08-08 13:58:42 +0200

    feat: expose pool item hooks through connection factory (#1)
    * feat: expose pool item hooks through connection factory
    
    * test: declare pool helper coverage

--- tests/connection-pool/Integration/ConnectionPoolFactoryTest.php
+++ tests/connection-pool/Integration/ConnectionPoolFactoryTest.php
@@ -13,10 +13,13 @@ use Allsilaevex\Pool\PoolItemWrapper;
 use PHPUnit\Framework\Attributes\UsesClass;
 use Allsilaevex\Pool\PoolItemWrapperFactory;
 use PHPUnit\Framework\Attributes\CoversClass;
+use Allsilaevex\Pool\Hook\PoolItemHookManager;
 use Allsilaevex\Pool\PoolItemFactoryInterface;
 use Allsilaevex\Pool\TimerTask\TimerTaskScheduler;
 use Allsilaevex\ConnectionPool\ConnectionPoolFactory;
 use Allsilaevex\ConnectionPool\Tasks\ResizerTimerTask;
+use Allsilaevex\ConnectionPool\Hooks\ConnectionCheckHook;
+use Allsilaevex\ConnectionPool\Hooks\ConnectionResetHook;
 use Allsilaevex\ConnectionPool\Tasks\LeakDetectionTimerTask;
 use Allsilaevex\Pool\TimerTask\TimerTaskSchedulerAwareTrait;
 use Allsilaevex\ConnectionPool\Tasks\PoolItemUpdaterTimerTask;
@@ -32,6 +35,9 @@ use Allsilaevex\ConnectionPool\Tasks\PoolItemUpdaterTimerTask;
 #[UsesClass(PoolItemWrapperFactory::class)]
 #[UsesClass(PoolItemUpdaterTimerTask::class)]
 #[UsesClass(TimerTaskSchedulerAwareTrait::class)]
+#[UsesClass(PoolItemHookManager::class)]
+#[UsesClass(ConnectionCheckHook::class)]
+#[UsesClass(ConnectionResetHook::class)]
 class ConnectionPoolFactoryTest extends TestCase
 {
     public function testInstantiate(): void
@@ -51,5 +57,31 @@ class ConnectionPoolFactoryTest extends TestCase
         $connectionFromPool = $pool->borrow();
 
         static::assertEquals($connection->id, $connectionFromPool->id);
+    }
+
+    public function testPoolItemHooksAreCombinedWithConnectionCheckers(): void
+    {
+        $connection = new stdClass();
+        $events = [];
+
+        $poolItemFactoryInterfaceMock = $this->createMock(PoolItemFactoryInterface::class);
+        $poolItemFactoryInterfaceMock->method('create')->willReturn($connection);
+        $poolItemFactoryInterfaceMock->method('destroy');
+
+        $connectionPoolFactory = new ConnectionPoolFactory(size: 1, factory: $poolItemFactoryInterfaceMock);
+        $connectionPoolFactory->addConnectionChecker(static function (object $connection) use (&$events): bool {
+            $events[] = 'before_borrow';
+
+            return true;
+        });
+        $connectionPoolFactory->addPoolItemHook(new ConnectionResetHook(static function (object $connection) use (&$events): void {
+            $events[] = 'after_return';
+        }));
+
+        $pool = $connectionPoolFactory->instantiate();
+        $connectionFromPool = $pool->borrow();
+        $pool->return($connectionFromPool);
+
+        static::assertSame(['before_borrow', 'after_return'], $events);
     }
 }
